aa5410905702bdf9f39a97d6d51ac2771634e4b7
Juan Ramirez <juanchocabrera2001@gmail.com>
2022-09-13 11:05:02 -0500

    POO: Añadiendo getter y setter

--- POO_js/src/modules/cursos.js
+++ POO_js/src/modules/cursos.js
@@ -12,13 +12,28 @@ class Cursos {
     name,
     teacher,
     clases = []
-
   }) {
     this.id = id
-    this.name = name
+    // Utilizamos por convención el guión bajo, para indicar que estamos usando el encapsulamiento,
+    this._name = name
     this.teacher = teacher
     this.clases = clases
   }
+
+  get name() {
+    //La sintaxis get (getter) vincula una propiedad de objeto a una función que se llamará cuando se busque esa propiedad.
+    return this._name
+  }
+
+    // La sintaxis set (setter) vincula una propiedad de objeto a una función que se llamará cuando haya un intento de establecer un nuevo valor.
+  set name(newNameOfCourse) {
+    if (newNameOfCourse !== this._name) {
+      console.error('No puedes modificar este campo')
+    } else {
+      this._name = newNameOfCourse
+    }
+  }
+
 }
 
 // Creando instancias
